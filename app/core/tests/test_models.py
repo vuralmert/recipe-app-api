da4c43890bdba2418e3da30dc1bd409eedc6fb6b
Mert <100792959+vuralmert@users.noreply.github.com>
2022-08-02 13:38:01 +0300

    Added test for user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,16 +4,17 @@ Test for models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTest(TestCase):
     """Test models"""
 
     def test_create_user_with_email_successful(self):
-        """Test creating a user with an email is succsesful"""
+        """Test creating a user with an email is successful"""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password,
+            email=email,
+            password=password,
         )
 
         self.assertEqual(user.email, email)
