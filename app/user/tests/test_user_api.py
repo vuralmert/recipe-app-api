cacb13d33a22468556abf7c9a27b97bf6b0f5485
Mert <100792959+vuralmert@users.noreply.github.com>
2022-08-04 11:16:21 +0300

    Fixed typo

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -21,7 +21,7 @@ def create_user(**params):
 class PublicUserApiTests(TestCase):
     """Test the public features of the user API"""
     def setUp(self):
-        self.client = APIClient
+        self.client = APIClient()
 
     def test_create_user_success(self):
         """Test creating a user is successful"""
