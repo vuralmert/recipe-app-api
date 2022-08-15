f310dd8a13c932fa81c4ace97c0bd9a8327bb766
Mert <100792959+vuralmert@users.noreply.github.com>
2022-08-15 12:32:07 +0300

    Added ingredient model

--- app/core/models.py
+++ app/core/models.py
@@ -57,6 +57,7 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient')
 
     def __str__(self):
         return self.title
@@ -69,6 +70,18 @@ class Tag(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         # If user is deleted, tags associated to that user will also be deleted
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient for recipes"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
     )
 
     def __str__(self):
